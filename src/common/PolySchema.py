5287abdcb2d9df240eb717bf7e94c9bc4dfaf72c
Michael Evans <amike.evans@gmail.com>
2024-06-24 13:33:48 -0400

    add optional width field

--- src/common/PolySchema.py
+++ src/common/PolySchema.py
@@ -1,11 +1,11 @@
 from marshmallow import Schema, fields, validate
 
-class FigureSizeSchema(Schema):
-    width = fields.Int(validate=validate.Range(min=0))
-    height = fields.Int(validate=validate.Range(min=0))
+# class FigureSizeSchema(Schema):
+#     width = fields.Int(validate=validate.Range(min=0))
 
 class PolySchema(Schema):
-    size = fields.Nested(FigureSizeSchema, required=False)
+    # size = fields.Nested(FigureSizeSchema, required=False)
     xVarNames = fields.List(fields.Str, required=True)
     yVarName = fields.Str(required=True)
     polynomial = fields.Int(required=True)
+    width = fields.Int(validate=validate.Range(min=1), required=False)
