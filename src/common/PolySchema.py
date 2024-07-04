954c2761c600537d0354d9268ab185347dc7df98
Michael Evans <amike.evans@gmail.com>
2024-07-04 16:30:05 -0400

    remove old schema

--- src/common/PolySchema.py
+++ src/common/PolySchema.py
@@ -1,10 +1,6 @@
 from marshmallow import Schema, fields, validate
 
-# class FigureSizeSchema(Schema):
-#     width = fields.Int(validate=validate.Range(min=0))
-
 class PolySchema(Schema):
-    # size = fields.Nested(FigureSizeSchema, required=False)
     xVarNames = fields.List(fields.Str, required=True)
     yVarName = fields.Str(required=True)
     polynomial = fields.Int(required=True)
